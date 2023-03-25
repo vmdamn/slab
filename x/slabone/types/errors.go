3ae7a7d1b1bc1fae16152eae9fd67299f9f2f781
vmdamn <mahajan.vishal.86@gmail.com>
2023-03-25 00:43:17 +0000

    types events  and errors

--- x/slabone/types/errors.go
+++ x/slabone/types/errors.go
@@ -7,6 +7,11 @@ import (
 )
 
 // x/slabone module sentinel errors
+
 var (
 	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrWrongSlabState = sdkerrors.Register(ModuleName, 5100, "It is an inappropriate current Slab state.") //Only Created Slabs can be Vetted and Vetted Slabs can be Revoked.
+	ErrVetterIsOriginator = sdkerrors.Register(ModuleName, 5200, "Is an Originator. Vetter can't be Originator")
+	ErrNotDirectedTowards = sdkerrors.Register(ModuleName, 5300, "Is where it should be directed towards. Not directed towards the Vetter")
+	ErrRevokerShouldBeVetter = sdkerrors.Register(ModuleName, 5400, "Should be Revoker i.e. Vetter Should Be Revoker")
 )
--- x/slabone/types/keys.go
+++ x/slabone/types/keys.go
@@ -22,3 +22,27 @@ const (
 	SlabKey      = "Slab/value/"
 	SlabCountKey = "Slab/count/"
 )
+
+const (
+    SlabCreatedEventOriginator =	"EvCreatedOriginator"
+	SlabCreatedEventOriginatorSocialId = "EvCreatedOriginatorSocialId"      
+    SlabCreatedEventSlabId 			   ="EvCreatedSlabId"       
+    SlabCreatedEventAssertion 		="EvCreatedAssertion"            
+	SlabCreatedEventDirectedTowards ="EvCreatedDirectedTowards"
+	SlabCreatedEventSlabState		= "EvCreatedSlabState"   
+	SlabCreatedEventSlabCtxTime		= "EvCreatedSlabCtxTime"
+	
+	SlabVettedEventVetter 			= "EvVettedVetter"
+	SlabVettedEventVetterSocialId   = "EvVettedVetterSocialId"
+	SlabVettedEventSlabState 		= "EvVettedSlabState"
+	SlabVettedEventSlabVettingNote	= "EvVettedVettingNote"
+	SlabVettedEventSlabId 			= "EvVettedSlabId" 
+	SlabVettedEventSlabCtxTime		= "EvVettedSlabCtxTime"
+
+	SlabRevokedEventRevoker			= "EvRevokedRevoker"
+	SlabRevokedEventRevokerSocialId = "EvRevokedRevokerSocialId"
+	SlabRevokedEventSlabState		= "EvRevokedSlabState"
+	SlabRevokedEventSlabRevokingNote= "EvRevokedRevokingNote"
+	SlabRevokedEventSlabId			= "EvRevokedSlabId"
+	SlabRevokedEventSlabCtxTime		= "EvRevokedSlabCtxTime"
+)	
