69d1091548226cc351c47913188b40a3e9bc6b54
vmdamn <mahajan.vishal.86@gmail.com>
2023-03-25 00:34:12 +0000

    msg server create-slab

--- x/slabone/keeper/msg_server_create_slab.go
+++ x/slabone/keeper/msg_server_create_slab.go
@@ -5,13 +5,57 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"slabone/x/slabone/types"
+	"strconv"
 )
 
 func (k msgServer) CreateSlab(goCtx context.Context, msg *types.MsgCreateSlab) (*types.MsgCreateSlabResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
 
-	return &types.MsgCreateSlabResponse{}, nil
+	bHeight := uint64(ctx.BlockHeight())
+	convbHeight := strconv.FormatUint(bHeight,10)
+
+	bTime := (ctx.BlockTime())
+    convbTime := bTime.String()
+
+	var slab = types.Slab{
+		OriginatorSocialId: msg.OriginatorSocialId,
+		OriginatorChainAddr:msg.Creator,
+		DirectedTowards: 	msg.DirectedTowards,
+		Assertion:		   	msg.Assertion,
+		UriOriginator:		msg.UriOriginator,
+		State:				"Created",
+		OriginatedCtxHeight:convbHeight,
+		OriginatedCtxTime: 	convbTime,
+		VetterSocialId:		"",
+		VetterChainAddr:	"",
+		VettingCtxHeight:	"",
+		VettingCtxTime: 	"",
+		VettingNote: 		"",
+		UriVetter:			"",
+		RevokingCtxHeight:	"",
+		RevokingCtxTime:	"",
+		RevokingNote:		"",
+		UriRevoker:			"",
+	}
+	
+	id := k.AppendSlab(ctx, slab)
+	
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(types.SlabCreatedEventOriginator, msg.Creator),
+			sdk.NewAttribute(types.SlabCreatedEventOriginatorSocialId, msg.OriginatorSocialId),
+			sdk.NewAttribute(types.SlabCreatedEventAssertion, msg.Assertion),
+			sdk.NewAttribute(types.SlabCreatedEventDirectedTowards, msg.DirectedTowards),
+			sdk.NewAttribute(types.SlabCreatedEventSlabState, slab.State),
+			sdk.NewAttribute(types.SlabCreatedEventSlabId, strconv.FormatUint(id,10)),
+			sdk.NewAttribute(types.SlabCreatedEventSlabCtxTime, slab.OriginatedCtxTime),
+		),
+	)
+
+
+
+	return &types.MsgCreateSlabResponse{Id: id}, nil
 }
