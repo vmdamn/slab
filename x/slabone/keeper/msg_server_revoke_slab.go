b297d79033d0ad0530fcbde3180a99336c8b7def
vmdamn <mahajan.vishal.86@gmail.com>
2023-03-25 00:38:30 +0000

    msg server revoke-slab

--- x/slabone/keeper/msg_server_revoke_slab.go
+++ x/slabone/keeper/msg_server_revoke_slab.go
@@ -2,16 +2,65 @@ package keeper
 
 import (
 	"context"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strconv"
 	"slabone/x/slabone/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) RevokeSlab(goCtx context.Context, msg *types.MsgRevokeSlab) (*types.MsgRevokeSlabResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	bHeight := uint64(ctx.BlockHeight())
+	convbHeight := strconv.FormatUint(bHeight,10)
+
+	bTime := (ctx.BlockTime())
+    convbTime := bTime.String()
+
+
+	slab, found := k.GetSlab(ctx, msg.Id)
+	
+	if !found {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrKeyNotFound, "key %d doesn't exist", msg.Id)		
+	}
+
+	if slab.State != "Vetted" {
+		return nil, sdkerrors.Wrapf(types.ErrWrongSlabState, "%v", slab.State)
+	}
+
+	if slab.VetterSocialId != msg.RevokerSocialId {
+		return nil, sdkerrors.Wrapf(types.ErrRevokerShouldBeVetter, "%s", slab.VetterSocialId)
+	}
+
+	if slab.VetterChainAddr != msg.Creator {
+		return nil, sdkerrors.Wrapf(types.ErrRevokerShouldBeVetter, "%s", slab.VetterChainAddr)
+	}
+
+	
+
+	slab.RevokerSocialId = msg.RevokerSocialId
+	slab.RevokerChainAddr = msg.Creator
+	slab.State = "Revoked"
+	slab.RevokingCtxHeight = convbHeight
+	slab.RevokingCtxTime = convbTime
+	slab.RevokingNote = msg.RevokingNote
+	slab.UriRevoker = msg.UriRevoker
+
+
+	k.SetSlab(ctx,slab)
+
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(types.SlabRevokedEventRevoker, msg.Creator),
+			sdk.NewAttribute(types.SlabRevokedEventRevokerSocialId, msg.RevokerSocialId),
+			sdk.NewAttribute(types.SlabRevokedEventSlabState, slab.State),
+			sdk.NewAttribute(types.SlabRevokedEventSlabRevokingNote, slab.RevokingNote),
+			sdk.NewAttribute(types.SlabRevokedEventSlabId, strconv.FormatUint(msg.Id,10)),
+			sdk.NewAttribute(types.SlabRevokedEventSlabCtxTime, slab.RevokingCtxTime),
+		),
+	)
 
 	return &types.MsgRevokeSlabResponse{}, nil
 }
